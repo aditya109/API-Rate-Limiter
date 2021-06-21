2848fc4195b5559dc1785af7c032e07a89764e2c
Aditya Kumar <adikid1996@gmail.com>
2021-06-21 22:54:37 +0530

    completed #1; implemented simple flask server;
    Signed-off-by: Aditya Kumar <adikid1996@gmail.com>

--- server/server.py
+++ server/server.py
@@ -1,11 +1,16 @@
-# Importing flask module in the project is mandatory
-# An object of Flask class is our WSGI application.
+import os
+import logging
 from flask import Flask
-  
+
 # Flask constructor takes the name of 
 # current module (__name__) as argument.
 app = Flask(__name__)
-  
+# creating a logger instance
+FORMAT = '%(asctime)s - %(levelname)s : %(message)s'
+logging.basicConfig(filemode='w',
+                    format=FORMAT)
+
+
 # The route() function of the Flask class is a decorator, 
 # which tells the application which URL should call 
 # the associated function.
@@ -13,10 +18,19 @@ app = Flask(__name__)
 # ‘/’ URL is bound with hello_world() function.
 def hello_world():
     return 'Hello World'
-  
+
+
+@app.route('/result')
+# '/result' URL is bound with get_result() function.
+def get_result():
+    return 'Received result'
+
+
 # main driver function
 if __name__ == '__main__':
-  
-    # run() method of Flask class runs the application 
+    # run() method of Flask class runs the application
     # on the local development server.
-    app.run()
+    host = "0.0.0.0"
+    port = 5000
+    logging.info('Starting server on {0} on PORT: {1}'.format(host, port))
+    app.run(host=host, port=port, debug=False)
